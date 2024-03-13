08d7d76db0fc38d5a99986f61b8e9f65e3ecd3c9
correaa1 <bruno_cs31@hotmail.com>
2024-03-13 14:56:29 -0300

    remove next export

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,20 +4,23 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { AuthProvider } from '../contexts/AuthContext'
 import RootLayout from '../../layouts/RootLayout/RootLayout'
 import theme from '../services/_theme'
+import { I18nextProvider } from 'react-i18next';
 import i18n from '../services/i18n' // Importe a configuração do i18n
 import MyComponent from '../services/i18n'
 import { useEffect, useState } from 'react'
 
 
 export default function App ({ Component, pageProps }) {
-  
-  return (
-      <ChakraProvider theme={theme}>
-          <AuthProvider>
-              <RootLayout>
-                  <Component {...pageProps} />
-              </RootLayout>
-          </AuthProvider>
-      </ChakraProvider>
-  );
+
+    return (
+        <ChakraProvider theme={theme}>
+            <I18nextProvider i18n={MyComponent}>
+                <AuthProvider>
+                    <RootLayout>
+                        <Component {...pageProps} />
+                    </RootLayout>
+                </AuthProvider>
+            </I18nextProvider>
+        </ChakraProvider>
+    )
 }
